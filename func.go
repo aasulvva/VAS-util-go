a42d422b6148d41555f23eb641cb8a7d799966cf
Åsulv Vaa <aasulvv@stud.ntnu.no>
2024-06-28 13:28:42 +0200

    Added functions to generate RSA PEM data

--- func.go
+++ func.go
@@ -1,6 +1,12 @@
 package VAS_util_go
 
-import "net"
+import (
+	"bytes"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"net"
+)
 
 // Extracts the IP from a string containing both port and IP
 func ExtractIP(remote string) string {
@@ -9,4 +15,28 @@ func ExtractIP(remote string) string {
 		return ""
 	}
 	return host
+}
+
+func GeneratePrivatePEM(privateKey *rsa.PrivateKey) []byte {
+	keyPem := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	}
+
+	buf := new(bytes.Buffer)
+
+	pem.Encode(buf, keyPem)
+	return buf.Bytes()
+}
+
+func GeneratePublicPEM(publicKey *rsa.PublicKey) []byte {
+	keyPem := &pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: x509.MarshalPKCS1PublicKey(publicKey),
+	}
+
+	buf := new(bytes.Buffer)
+
+	pem.Encode(buf, keyPem)
+	return buf.Bytes()
 }
