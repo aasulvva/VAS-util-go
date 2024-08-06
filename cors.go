28ba5ec1d209be8a56bd2f18532a116c8ae62590
Åsulv Vaa <aasulvv@stud.ntnu.no>
2024-08-06 20:13:50 +0200

    Added additional CORS-related headers

--- cors.go
+++ cors.go
@@ -4,4 +4,6 @@ import "net/http"
 
 func enableCors(w *http.ResponseWriter, corsUrl string) {
 	(*w).Header().Set("Access-Control-Allow-Origin", corsUrl)
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "*")
 }
